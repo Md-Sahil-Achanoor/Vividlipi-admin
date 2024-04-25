7c3cb879631285da3337186ea60e075a6f9b9cd1
MofasserHossain <mofasserhossain01@gmail.com>
2024-04-25 14:55:52 +0600

    [feature](env):env modified

--- src/constants/service/index.ts
+++ src/constants/service/index.ts
@@ -2,4 +2,6 @@ const baseURL = import.meta.env.VITE_API_URL;
 
 const projectName = import.meta.env.VITE_APP_NAME;
 
-export { baseURL, projectName };
+const hashSecret = import.meta.env.VITE_APP_HASH_SECRETE;
+
+export { baseURL, hashSecret, projectName };
